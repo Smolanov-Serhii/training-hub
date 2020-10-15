0c1452f061e43d14fa54344591434b506b7013b4
Smolanov-Sergey <smolanovsergey@gmail.com>
2020-10-15 11:01:50 +0300

    Главная страница, вёрстка блоков

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,13 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             draggable: true,
+            fade: true,
             touchMove: true,
             // prevArrow: $('.nav_icon_prev'),
             // nextArrow: $('.nav_icon_next'),
             swipe: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
             adaptiveHeight: true
         });
     });
